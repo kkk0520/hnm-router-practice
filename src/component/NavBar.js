b81b86238dbd0e428682c1fd99e5f183a92198f8
Jiyoung Kim <kjy0520@gmail.com>
2025-04-09 01:34:23 +0900

    css 포기

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -8,23 +8,27 @@ const NavBar = () => {
 
   return (
     <div>
-        <div className="login-button">
-            <div className="login-user"><FontAwesomeIcon icon={faUser} /></div>
-            <div>로그인</div>
-        </div>
-        <div className="nav-section">
-            <img width={150} src="https://th.bing.com/th/id/R.a280ff18ccf4ffddcc7aa3844246ca28?rik=nwrwTuniDz7fyA&riu=http%3a%2f%2fphotos.prnewswire.com%2fprnfull%2f20131009%2fNY95232LOGO&ehk=oyYBCOaR7y9%2fryZSj1hWWeh6uzJUT5PfkSEnr4tCc%2bw%3d&risl=&pid=ImgRaw&r=0" />
-        </div>
-        <div className="menu-area">
-            <ul className="menu-list">
-                {menuList.map((menu) => (
-                    <li>{menu}</li>
-                ))}                
-            </ul>
-            <div className="nav-search">
-                <FontAwesomeIcon icon={faSearch}/>
-                <input type="text"></input>
-
+        <div>
+            <div className="login-button">
+                <div className="login-user"><FontAwesomeIcon icon={faUser} /></div>
+                <div>로그인</div>
+            </div>
+            
+            <div className="nav-section">
+                <img width={100} src="https://th.bing.com/th/id/R.a280ff18ccf4ffddcc7aa3844246ca28?rik=nwrwTuniDz7fyA&riu=http%3a%2f%2fphotos.prnewswire.com%2fprnfull%2f20131009%2fNY95232LOGO&ehk=oyYBCOaR7y9%2fryZSj1hWWeh6uzJUT5PfkSEnr4tCc%2bw%3d&risl=&pid=ImgRaw&r=0" />
+            </div>
+            <div className="menu-area">
+                <div>
+                    <ul className="menu-list">
+                        {menuList.map((menu) => (
+                            <li>{menu}</li>
+                        ))}                
+                    </ul>
+                </div>
+                <div className="nav-search">
+                    <FontAwesomeIcon icon={faSearch}/>
+                    <input type="text"></input>
+                </div>            
             </div>
         </div>
     </div>
