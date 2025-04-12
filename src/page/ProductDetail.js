7c077b6114627a38752be5d817506f5d7bb587f0
Jiyoung Kim <kjy0520@gmail.com>
2025-04-13 01:19:38 +0900

    왜 갑자기 로컬호스트로 되어있니

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -10,7 +10,7 @@ const ProductDetail = () => {
   const[product, setProduct] = useState(null)
   const [selectedSize, setSelectedSize] = useState(null);
   const getProductDetail = async ()=>{
-    let url = `http://localhost:5000/products/${id}`
+    let url = `https://my-json-server.typicode.com/kkk0520/hnm-router-practice/products/${id}`
     let response = await fetch(url)
     let data = await response.json()
     console.log(data)
